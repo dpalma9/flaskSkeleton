465071ca5f0061ab5ace5a564c3dc4fb76b5e746
DaniHome <danypalma.cc@gmail.com>
2025-04-04 10:32:00 +0200

    debug mode is set on config

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,6 @@ from .routes import bp as routes
 
 def create_app():
     app = Flask(__name__)
-    #app.config.from_mapping(
-    #    SECRET_KEY='your_secret_key',
-    #)
 
     app.register_blueprint(routes)
 
--- run.py
+++ run.py
@@ -6,4 +6,4 @@ app = create_app()
 app.config.from_object(DevelopmentConfig)
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000)
