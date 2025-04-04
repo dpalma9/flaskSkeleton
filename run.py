680f2a9264004ca98bfd201f2f960a52d10a6c29
DaniHome <danypalma.cc@gmail.com>
2025-04-04 11:53:26 +0200

    load config profile via env var

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
+import os
+from dotenv import load_dotenv
 from app import create_app
-from app.config import Config, DevelopmentConfig
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 app = create_app()
-#app.config.from_object(Config)
-app.config.from_object(DevelopmentConfig)
+app.config.from_object(os.getenv('APP_SETTINGS', 'app.config.ProductionConfig').strip('"').strip("'"))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
